e475ee6b4a5e1c223ffcaf30915456bb6a52df8c
Álvaro Castro-Castilla <alvaro@castrocastilla.com>
2010-05-01 19:12:47 +0200

    Proper debugging flag

--- cyma/__deploy/build.sh
+++ cyma/__deploy/build.sh
@@ -3,5 +3,5 @@
 declare -x PATH_TO_SCRIPT=`dirname $0`
 
 cd $PATH_TO_SCRIPT
-xfbuild ../application/Main.d +h +noop +full +cdmd +obin/Cyma -I../../.. -I../../ -g -w -debug -debug=verbose -L-lGL -L-lXrandr
+xfbuild ../application/Main.d +h +noop +full +cdmd +obin/Cyma -I../../.. -I../../ -gc -w -debug -debug=verbose -L-lGL -L-lXrandr
 if [[ `ls *di 2> /dev/null` ]]; then rm -R *di; fi;
